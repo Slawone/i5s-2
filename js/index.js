bce33cdb64b16111a9ba6efe58b67391de34bf1f
Slawone <for1s8@ya.ru>
2025-03-27 20:49:29 +0300

    fix mobile contacts

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
-let acc = document.querySelectorAll('.accordion');
+import './acc.js';
+import scrollSection from './scrollSection.js';
 
-acc.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    if (e.target.classList.contains('active')) {
-      e.target.classList.remove('active');
-      return;
-    }
-    acc.forEach((e) => e.classList.remove('active'));
-    e.target.classList.add('active');
-  });
-});
+const menuLinks = document.querySelectorAll('[data-btn="menuLink"]');
+
+menuLinks.forEach((item) => item.addEventListener('click', scrollSection));
